438a99cee5b8f7fe2ba5a59b3bfabd1cc663577f
sadik182 <sadikurrahman728@gmail.com>
2022-08-24 00:12:25 +0600

    s

--- src/component/Portfolio/Portfolio.js
+++ src/component/Portfolio/Portfolio.js
@@ -1,49 +1,67 @@
-import React, { useState } from "react"
-import "./Portfolio.css"
-import Card from "./Card"
-import Portfolio_data from "./Portfolio_data"
-import PortfolioList from "./PortfolioList"
+import React, { useState } from "react";
+import "./Portfolio.css";
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import image from '../../images/portfolio_2.jpg'
+import { faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faExternalLink } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Portfolio = () => {
-  const [selected, setSelected] = useState('react')
-  const list = [
-    {
-      id: 'react',
-      title: 'React Project'
-    },
-    {
-      id: 'javaScript',
-      title: 'JavaScript Project'
-    },
-    {
-      id: 'frontend',
-      title: 'Frontend Project'
-    },
-    {
-      id: 'backend',
-      title: 'Backend Project'
-    },
-  ]
-  
   return (
     <>
-      <section className='Portfolio top' id='portfolio'>
-        <div className='container'>
-          <div className='heading text-center '>
+      <section className="Portfolio top" id="portfolio">
+        <div className="container">
+          <div className="heading text-center ">
             <h4>VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK</h4>
             <h1>My Portfolio</h1>
-            <ul className="compunent">
-              
-            </ul>
+            <ul className="compunent"></ul>
           </div>
           <div>
-            
+            <CardGroup className="card_group">
+              <Card className="btn_shadow">
+                <Card.Img variant="top" src={image} className="cardImage"/>
+                <Card.Body>
+                  <Card.Title className="cardTitle">Card title</Card.Title>
+                </Card.Body>
+                <Card.Footer className='d-flex'>
+                <a href="https://github.com/Sadik182?tab=repositories" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x"/></a>
+            <a href="#" target="_blank"><FontAwesomeIcon icon={faExternalLink} size="2x"/></a>
+                </Card.Footer>
+              </Card>
+              <Card>
+              <Card.Img variant="top" src={image} />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This card has supporting text below as a natural lead-in to
+                    additional content.{" "}
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+              </Card>
+              <Card>
+              <Card.Img variant="top" src={image} />
+                <Card.Body>
+                  <Card.Title>Card title</Card.Title>
+                  <Card.Text>
+                    This is a wider card with supporting text below as a natural
+                    lead-in to additional content. This card has even longer
+                    content than the first to show that equal height action.
+                  </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                  <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+              </Card>
+            </CardGroup>
           </div>
-          
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
