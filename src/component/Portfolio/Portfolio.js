7f2c174abd66c6faeb3067ad6681a25cab0cc48c
sadik182 <sadikurrahman728@gmail.com>
2022-08-26 21:56:55 +0600

    portfolio page 50% done

--- src/component/Portfolio/Portfolio.js
+++ src/component/Portfolio/Portfolio.js
@@ -19,14 +19,14 @@ const Portfolio = () => {
           </div>
           <div>
             <CardGroup className="card_group">
-              <Card className="btn_shadow">
+              <Card className="btn_shadow2">
                 <Card.Img variant="top" src={image} className="cardImage"/>
                 <Card.Body>
-                  <Card.Title className="cardTitle">Card title</Card.Title>
+                  <Card.Title className="cardTitle">Doctors Portal Using MERN Stack</Card.Title>
                 </Card.Body>
                 <Card.Footer className='d-flex'>
-                <a href="https://github.com/Sadik182?tab=repositories" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x"/></a>
-            <a href="#" target="_blank"><FontAwesomeIcon icon={faExternalLink} size="2x"/></a>
+                <a className="gitHub" href="https://github.com/Sadik182?tab=repositories" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x" title="Source Code"/></a>
+            <a className="liveDemo" href="#" target="_blank"><FontAwesomeIcon icon={faExternalLink} size="2x" title="Live Demo"/></a>
                 </Card.Footer>
               </Card>
               <Card>
