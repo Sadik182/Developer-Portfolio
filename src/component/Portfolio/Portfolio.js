f7fb06c43b823fce7c0f40f9f340c71c361f5db1
sadik182 <sadikurrahman728@gmail.com>
2022-10-19 13:31:42 +0600

    Portfolio page done

--- src/component/Portfolio/Portfolio.js
+++ src/component/Portfolio/Portfolio.js
@@ -1,87 +1,48 @@
-import React from "react";
-import "./Portfolio.css";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import CardGroup from "react-bootstrap/CardGroup";
-import image from '../../images/portfolio_2.jpg'
-import { faGithub } from "@fortawesome/free-brands-svg-icons"
-import { faExternalLink } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import image from '../../images/portfolio_2.jpg';
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faExternalLink } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import './Portfolio.css'
+
+import data from './PortfolioData.js';
+
 
 const Portfolio = () => {
+  const [show, setShow] = useState(3);
+  const slice = data.slice(0, show);
+  const loadMore = () => {
+    setShow(show + show);
+  }
   return (
     <>
       <section className="Portfolio top" id="portfolio">
-        <div className="container1">
           <div className="heading text-center ">
             <h4>VISIT MY PORTFOLIO AND KEEP YOUR FEEDBACK</h4>
             <h1>My Portfolio</h1>
-            <ul className="compunent"></ul>
           </div>
-          <div>
-            <CardGroup className="card_group">
-              <Card className="btn_shadow2">
-                <Card.Img variant="top" src={image} className="cardImage"/>
-                <Card.Body>
-                  <Card.Title className="cardTitle">Doctors Portal Using MERN Stack</Card.Title>
-                </Card.Body>
-                <Card.Footer className='d-flex'>
-                <a className="gitHub" href="https://github.com/Sadik182?tab=repositories"><FontAwesomeIcon icon={faGithub} size="2x" title="Source Code"/></a>
-            <a className="liveDemo" href="https://github.com/Sadik182?tab=repositories"><FontAwesomeIcon icon={faExternalLink} size="2x" title="Live Demo"/></a>
-                </Card.Footer>
-              </Card>
-              <Card className="btn_shadow2">
-                <Card.Img variant="top" src={image} className="cardImage"/>
-                <Card.Body>
-                  <Card.Title className="cardTitle">Doctors Portal Using MERN Stack</Card.Title>
-                </Card.Body>
-                <Card.Footer className='d-flex'>
-                <a className="gitHub" href="https://github.com/Sadik182?tab=repositories" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x" title="Source Code"/></a>
-            <a className="liveDemo" href="#" target="_blank"><FontAwesomeIcon icon={faExternalLink} size="2x" title="Live Demo"/></a>
-                </Card.Footer>
-              </Card>
-              <Card className="btn_shadow2">
-                <Card.Img variant="top" src={image} className="cardImage"/>
-                <Card.Body>
-                  <Card.Title className="cardTitle">Doctors Portal Using MERN Stack</Card.Title>
-                </Card.Body>
-                <Card.Footer className='d-flex'>
-                <a className="gitHub" href="https://github.com/Sadik182?tab=repositories" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x" title="Source Code"/></a>
-            <a className="liveDemo" href="#" target="_blank"><FontAwesomeIcon icon={faExternalLink} size="2x" title="Live Demo"/></a>
-                </Card.Footer>
-              </Card>
-              <Card className="btn_shadow2">
-                <Card.Img variant="top" src={image} className="cardImage"/>
-                <Card.Body>
-                  <Card.Title className="cardTitle">Doctors Portal Using MERN Stack</Card.Title>
-                </Card.Body>
-                <Card.Footer className='d-flex'>
-                <a className="gitHub" href="https://github.com/Sadik182?tab=repositories" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x" title="Source Code"/></a>
-            <a className="liveDemo" href="#" target="_blank"><FontAwesomeIcon icon={faExternalLink} size="2x" title="Live Demo"/></a>
-                </Card.Footer>
-              </Card>
-              <Card className="btn_shadow2">
-                <Card.Img variant="top" src={image} className="cardImage"/>
-                <Card.Body>
-                  <Card.Title className="cardTitle">Doctors Portal Using MERN Stack</Card.Title>
-                </Card.Body>
-                <Card.Footer className='d-flex'>
-                <a className="gitHub" href="https://github.com/Sadik182?tab=repositories" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x" title="Source Code"/></a>
-            <a className="liveDemo" href="#" target="_blank"><FontAwesomeIcon icon={faExternalLink} size="2x" title="Live Demo"/></a>
-                </Card.Footer>
-              </Card>
-              <Card className="btn_shadow2">
-                <Card.Img variant="top" src={image} className="cardImage"/>
-                <Card.Body>
-                  <Card.Title className="cardTitle">Doctors Portal Using MERN Stack</Card.Title>
-                </Card.Body>
-                <Card.Footer className='d-flex'>
-                <a className="gitHub" href="https://github.com/Sadik182?tab=repositories" target="_blank"><FontAwesomeIcon icon={faGithub} size="2x" title="Source Code"/></a>
-            <a className="liveDemo" href="#" target="_blank"><FontAwesomeIcon icon={faExternalLink} size="2x" title="Live Demo"/></a>
-                </Card.Footer>
-              </Card>
-            
-            </CardGroup>
+        <div className="container1">
+          <div className="row justify-content-center align-items-center">
+          {
+            slice.map((item, index) => {
+              return (
+                <div className="btn_shadow2 col-12 col-lg-4 col-md-6 mb-4" key={index}>
+                  <img src={item.img} alt="" className="cardImage" />
+                   <h3 className="cardTitle">{item.title}</h3>
+                  <div className="d-flex">
+                   <a className="gitHub" href={item.git}><FontAwesomeIcon icon={faGithub} size="2x" title="Source Code"/></a>
+                   <a className="liveDemo" href={item.live}><FontAwesomeIcon icon={faExternalLink} size="2x" title="Live Demo"/></a>
+                  </div>
+               </div>
+              )
+            })
+          }
           </div>
+          {
+            show < 6 ? (<button onClick={loadMore} className="btn btn-dark d-block w-100">Load More</button>): (null)
+          }
         </div>
       </section>
     </>
---
+++ src/component/Portfolio/PortfolioData.js
@@ -0,0 +1,48 @@
+import image from '../../images/portfolio_2.jpg';
+
+const cardData = [
+        {
+            id: 1,
+            img: image,
+            title: 'Doctors Portal Using MERN Stack',
+            git: 'https://github.com/Sadik182?tab=repositories',
+            live: 'https://github.com/Sadik182?tab=repositories'
+        },
+        {
+            id: 2,
+            img: image,
+            title: 'Doctors Portal Using MERN Stack',
+            git: 'https://github.com/Sadik182?tab=repositories',
+            live: 'https://github.com/Sadik182?tab=repositories'
+        },
+        {
+            id: 3,
+            img: image,
+            title: 'Doctors Portal Using MERN Stack',
+            git: 'https://github.com/Sadik182?tab=repositories',
+            live: 'https://github.com/Sadik182?tab=repositories'
+        },
+        {
+            id: 4,
+            img: image,
+            title: 'Doctors Portal Using MERN Stack',
+            git: 'https://github.com/Sadik182?tab=repositories',
+            live: 'https://github.com/Sadik182?tab=repositories'
+        },
+        {
+            id: 5,
+            img: image,
+            title: 'Doctors Portal Using MERN Stack',
+            git: 'https://github.com/Sadik182?tab=repositories',
+            live: 'https://github.com/Sadik182?tab=repositories'
+        },
+        {
+            id: 6,
+            img: image,
+            title: 'Doctors Portal Using MERN Stack',
+            git: 'https://github.com/Sadik182?tab=repositories',
+            live: 'https://github.com/Sadik182?tab=repositories'
+        },
+    ]
+
+    export default cardData;
