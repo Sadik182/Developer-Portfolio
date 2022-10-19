b6e77cf51dbecbc8c2ca28f1d96f809e457486dc
sadik182 <sadikurrahman728@gmail.com>
2022-10-19 19:00:52 +0600

    contact page done

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -2,7 +2,7 @@ import React, { useState } from "react"
 import contact1 from "./contact1.png"
 import "./Contact.css"
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome"
-import {faGithub, faFacebookF, faLinkedinIn, faMailchimp} from '@fortawesome/free-brands-svg-icons'
+import {faGithub, faFacebookF, faLinkedinIn, faMailchimp, faTelegram} from '@fortawesome/free-brands-svg-icons'
 import {faArrowRight, faPhone, faPhoneAlt, faMailBulk, faMailReply, faMailForward} from "@fortawesome/free-solid-svg-icons"
 const Contact = () => {
   const [data, setData] = useState({
@@ -37,9 +37,8 @@ const Contact = () => {
   return (
     <>
       <section className='Contact' id='contact'>
-        <div className='container top'>
+        <div className='container1 top'>
           <div className='heading text-center'>
-            <h4>CONTACT</h4>
             <h5>If You Have Any Project In Your Mind</h5>
             <h1>Contact With Me</h1>
           </div>
@@ -53,7 +52,7 @@ const Contact = () => {
                 <div className='details'>
                   <h1>Md. Sadikur Rahman</h1>
                   <h3>Jr. Software Engineer</h3>
-                  <p>I am available for freelance work. If you have any project in your mind Connect with me via and call in to my account. I will make it soon as possible.</p> <br />
+                  <p className="text-justify">I am available for freelance work. If you have any project in your mind Connect with me via and call in to my account. I will make it soon as possible.</p> <br />
                   <p><FontAwesomeIcon icon={faPhone} /> (+88) 01715925172</p>
                   <p><FontAwesomeIcon icon={faMailBulk} /> Sadikurrahman728@gmail.com</p> <br />
                   <span>FIND WITH ME</span>
@@ -67,6 +66,9 @@ const Contact = () => {
                     <button className='btn_shadow'>
                     <a href="https://github.com/Sadik182 "><FontAwesomeIcon icon={faGithub} size="2x"/></a>
                     </button>
+                    <button className='btn_shadow'>
+                    <a href="https://t.me/Sadik1820"><FontAwesomeIcon icon={faTelegram} size="2x"/></a>
+                    </button>
                   </div>
                 </div>
               </div>
@@ -74,12 +76,12 @@ const Contact = () => {
 
             <div className='right box_shodow'>
               <form onSubmit={formSubmit}>
-                <div className='f_flex'>
-                  <div className='input row'>
+                <div className='d-flex w-100'>
+                  <div className='w-100'>
                     <span>YOUR NAME</span>
                     <input type='text' name='fullname' value={data.fullname} onChange={InputEvent} />
                   </div>
-                  <div className='input row'>
+                  <div className='w-100'>
                     <span>PHONE NUMBER </span>
                     <input type='number' name='phone' value={data.phone} onChange={InputEvent} />
                   </div>
@@ -96,7 +98,7 @@ const Contact = () => {
                   <span>YOUR MESSAGE </span>
                   <textarea cols='30' rows='10' name='message' value={data.message} onChange={InputEvent}></textarea>
                 </div>
-                <button className='btn_shadow'>
+                <button className='btn_shadow mt-3'>
                   SEND MESSAGE <FontAwesomeIcon icon={faArrowRight} size = "1x"/>
                 </button>
               </form>
